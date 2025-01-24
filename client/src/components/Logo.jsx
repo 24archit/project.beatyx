22150812b4d95cfa617f75ef611ca4e98ccceb8e
Archit Mishra <architmishra016@gmail.com>
2025-01-24 18:33:48 +0530

    Rebranded the logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,15 +1,12 @@
-import '../assets/styles/Logo.css'
-import {    Link } from 'react-router-dom';
+import '../assets/styles/Logo.css';
+import { Link } from 'react-router-dom';
+import LogoImg from "../assets/media/logo.png"
 export default function Logo() {
-    return (
-            <Link to="/" className="logo">
-            <div className="logo-symbol">
-                <h1 id="logo-symbol">&#119073;</h1>
-            </div>
-            <div className="logo-name">
-                <h1 id="logo-name">HarmoniX</h1>
-                <p id="slogan">Music To Your Mood</p>
-            </div>
-        </Link>
-    );  
+  return (
+    <Link to="/" className="logo">
+      <div className="logo-symbol">
+        <img id="logo-symbol" src={LogoImg} alt="Logo" />
+      </div>
+    </Link>
+  );
 }
