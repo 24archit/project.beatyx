e193328e13f8f1e043bc7f6f66d8c9cbb9bcc89a
Archit Mishra <architmishra016@gmail.com>
2025-01-26 15:55:57 +0530

    added speed insights

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { inject } = require('@vercel/analytics');
 const express = require("express");
 const app = express();
 const compression = require("compression");
