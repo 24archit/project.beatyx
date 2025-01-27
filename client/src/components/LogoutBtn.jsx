ae10ff74735e417c63e3a70b24c9cfd6f7ca39ba
Archit Mishra <architmishra016@gmail.com>
2025-01-27 21:44:39 +0530

    Fixed an issue

--- client/src/components/LogoutBtn.jsx
+++ client/src/components/LogoutBtn.jsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import { styled } from "@mui/material/styles";
 
 // Define custom styled components
 const StyledDialog = styled(Dialog)(({ theme }) => ({
-  backdropFilter: 'blur(8px)',
-  backgroundColor: 'transparent', // Transparent to allow gradient effect
-  '& .MuiPaper-root': {
-    background: 'linear-gradient(#6223a96d, rgba(16, 108, 221, 0.405), #232ca96d, #6223a96d)', // Gradient background
+  backdropFilter: "blur(8px)",
+  backgroundColor: "transparent", // Transparent to allow gradient effect
+  "& .MuiPaper-root": {
+    background:
+      "linear-gradient(#6223a96d, rgba(16, 108, 221, 0.405), #232ca96d, #6223a96d)", // Gradient background
     borderRadius: 16, // Increased border radius
     padding: theme.spacing(2),
     boxShadow: theme.shadows[5],
@@ -19,35 +20,35 @@ const StyledDialog = styled(Dialog)(({ theme }) => ({
 }));
 
 const StyledDialogTitle = styled(Button)(({ theme }) => ({
-  color: 'lightGreen',
-  fontFamily: 'sans-serif',
-  fontSize: '2rem',
-  fontWeight: 'bold',
-  textTransform: 'none',
+  color: "lightGreen",
+  fontFamily: "sans-serif",
+  fontSize: "2rem",
+  fontWeight: "bold",
+  textTransform: "none",
 }));
 
 const LogoutButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText('#2196F3'), // Contrast text based on primary color
-  backgroundColor: '#ff4d4f', // Reddish color for logout button
-  '&:hover': {
-    backgroundColor: '#f5222d', // Darker shade on hover
+  color: theme.palette.getContrastText("#2196F3"), // Contrast text based on primary color
+  backgroundColor: "#ff4d4f", // Reddish color for logout button
+  "&:hover": {
+    backgroundColor: "#f5222d", // Darker shade on hover
   },
 }));
 
 const CancelButton = styled(Button)(({ theme }) => ({
-  color: 'rgb(5, 4, 28)', // Contrast text based on primary color
-  backgroundColor: 'rgb(164, 163, 194)', // Reddish color for logout button
-  '&:hover': {
-    backgroundColor: 'rgb(82, 81, 112)', // Darker shade on hover
+  color: "rgb(5, 4, 28)", // Contrast text based on primary color
+  backgroundColor: "rgb(164, 163, 194)", // Reddish color for logout button
+  "&:hover": {
+    backgroundColor: "rgb(82, 81, 112)", // Darker shade on hover
   },
 }));
 
 const StyledDialogContentText = styled(DialogContentText)(({ theme }) => ({
-  color: 'white',
-  fontFamily: 'Arial',
+  color: "white",
+  fontFamily: "Arial",
   fontWeight: 400,
-  textTransform: 'none', // Ensure text is not transformed to uppercase
-  fontSize: '1.3rem',
+  textTransform: "none", // Ensure text is not transformed to uppercase
+  fontSize: "1.3rem",
 }));
 
 export default function AlertDialog() {
@@ -63,11 +64,8 @@ export default function AlertDialog() {
 
   const handleLogout = async () => {
     try {
-      const clearCookie = async (name) => {
-        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-      };
-      await clearCookie('userdetails');
-      window.location.href = 'https://harmonix-play.vercel.app/login';
+      window.localStorage.clear();
+      window.location.href = "/";
     } catch (error) {
       console.error(error);
     }
@@ -75,8 +73,13 @@ export default function AlertDialog() {
 
   return (
     <React.Fragment>
-      <button id="logout-btn" className="log-in-out-btns" onClick={handleClickOpen}>
-        <i className="fa-solid fa-right-from-bracket" id="logout-icon"></i>&nbsp;Logout
+      <button
+        id="logout-btn"
+        className="log-in-out-btns"
+        onClick={handleClickOpen}
+      >
+        <i className="fa-solid fa-right-from-bracket" id="logout-icon"></i>
+        &nbsp;Logout
       </button>
       <StyledDialog
         open={open}
@@ -89,17 +92,25 @@ export default function AlertDialog() {
         </StyledDialogTitle>
         <DialogContent>
           <StyledDialogContentText id="alert-dialog-description">
-            You are about to logout. Are you sure?<br /><br />
-            Note: To abort the permissions given during authorization, visit <a href='https://www.spotify.com/in-en/account/apps/' target="_blank" rel="noopener noreferrer" style={{ color: "#e4a81db4", textDecoration: "underline" }}>Spotify Account</a>
+            You are about to logout. Are you sure?
+            <br />
+            <br />
+            Note: To abort the permissions given during authorization, visit{" "}
+            <a
+              href="https://www.spotify.com/in-en/account/apps/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#e4a81db4", textDecoration: "underline" }}
+            >
+              Spotify Account
+            </a>
           </StyledDialogContentText>
         </DialogContent>
         <DialogActions>
           <CancelButton onClick={handleClose} color="primary">
             Cancel
           </CancelButton>
-          <LogoutButton onClick={handleLogout}>
-            Logout
-          </LogoutButton>
+          <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
         </DialogActions>
       </StyledDialog>
     </React.Fragment>
