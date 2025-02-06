e7d8c95ce342b80ae8071b5212e7af3b6bd8ac26
Archit Mishra <architmishra016@gmail.com>
2025-02-06 17:32:45 +0530

    Modifies the track db model

--- server/models/tracksDb.js
+++ server/models/tracksDb.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+
 const trackSchema = new mongoose.Schema({
   spotifyId: {
     type: String,
+    unique: true, // Ensures spotifyId is unique
+    required: true, // Ensures spotifyId is mandatory
   },
   youtubeLink: {
     type: String,
