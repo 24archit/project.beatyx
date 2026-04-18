add6c21454a74bf93ace03d1475ba43ac321a8f0
Archit <architmishra016@gmail.com>
2026-04-18 18:40:30 +0530

    Fixed logo unload issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Clear cache on session close to prevent stale image loading issues
+window.addEventListener("beforeunload", () => {
+  if ("caches" in window) {
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name);
+      });
+    });
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
